1743c931a0479cd2735e7340611fda5986deb693
Thomas D <tdietert@mail.usf.edu>
2014-09-08 19:55:30 -0400

    Added UploadForm with WTForms

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask.ext.wtf import Form
 from app.models import User
-from wtforms import TextField, BooleanField, SubmitField, PasswordField, FileField
+from app import uploads
+from flask_wtf.file import FileField, file_allowed, file_required
+from wtforms import TextField, BooleanField, SubmitField, PasswordField
 from wtforms.validators import Required, EqualTo, DataRequired, Length
 
 class LoginForm(Form):
@@ -12,3 +14,6 @@ class RegisterForm(Form):
 	username = TextField("Team Name", [Required("Username Field Required"), Length(min=3, max=15)])
 	password = PasswordField('New Password', [Required("Password Field Required"), Length(min=5, max=20), EqualTo('confirm', message='Passwords must match')])
 	confirm = PasswordField('Repeat Password')
+
+class UploadForm(Form):
+	upload = FileField("Upload your solution:", [file_required(), file_allowed(uploads, "Please submit a file with a valid extension")])
